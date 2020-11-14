4803c0c1aed58fa16e1a47cb7581d15b4aed8cac
Arwyne De Guzman <arwynedeguzman@gmail.com>
2020-11-14 19:10:46 +0800

    added result and reset

--- lib/main.dart
+++ lib/main.dart
@@ -14,21 +14,46 @@ class _MyAppState extends State<MyApp> {
   final _questions = const [
     {
       "questionText": "What's your favorite color?",
-      "answers": ["Black", "Red", "Blue", "Yellow"]
+      "answers": [
+        {"text": "Black", "score": 10},
+        {"text": "Red", "score": 5},
+        {"text": "Blue", "score": 3},
+        {"text": "White", "score": 1}
+      ]
     },
     {
       "questionText": "What's your favorite animal?",
-      "answers": ["Dog", "Cat", "Snake", "Rabbit"]
+      "answers": [
+        {"text": "Cat", "score": 5},
+        {"text": "Snake", "score": 10},
+        {"text": "Dog", "score": 3},
+        {"text": "Rabbit", "score": 7}
+      ]
     },
     {
-      "questionText": "What's your favorite instructor?",
-      "answers": ["Max1", "Max2", "Max3", "Max4"]
+      "questionText": "What's your favorite sports?",
+      "answers": [
+        {"text": "Basketball", "score": 3},
+        {"text": "Volleyball", "score": 1},
+        {"text": "Baseball", "score": 5},
+        {"text": "Wrestling", "score": 8}
+      ]
     }
   ];
 
   var _questionIndex = 0;
+  var _totalScore = 0;
 
-  void _answerQuestion() {
+  void _resetQuiz() {
+    setState(() {
+      _questionIndex = 0;
+      _totalScore = 0;
+    });
+  }
+
+  void _answerQuestion(int score) {
+    _totalScore += score;
+
     setState(() {
       _questionIndex = _questionIndex + 1;
     });
@@ -53,7 +78,7 @@ class _MyAppState extends State<MyApp> {
                 answerQuestion: _answerQuestion,
                 questions: _questions,
                 questionIndex: _questionIndex)
-            : Result(),
+            : Result(_totalScore, _resetQuiz),
       ),
     );
   }
